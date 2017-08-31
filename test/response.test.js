5d2629f8399341988639d1eb8d6ccdfff9be1ec1
Stephan Schultz <register@steppschuh.net>
2017-08-31 09:12:59 +0200

    Created basic response parsing tests

--- test/response.test.js
+++ test/response.test.js
@@ -7,15 +7,25 @@
 var should = require('chai').should();
 var {Response} = require('../lib/response');
 
-describe('Response', function() {
-  it('Parses reseponse object from string', function() {
-  	let responseString = "{\r\n    \"statusCode\": 200,\r\n    \"statusMessage\": \"OK\",\r\n    \"lastUpdated\": 1504128373543,\r\n    \"content\": []\r\n}";
-  	//console.log(`Parsing response string:\n${responseString}`);
-  	let response = Response.fromString(responseString);
-  	//console.log(`Parsed response object:\n${response.statusCode}`);
+describe('Response Parsing', function() {
+
+	const responseString = "{\r\n    \"statusCode\": 200,\r\n    \"statusMessage\": \"OK\",\r\n    \"lastUpdated\": 1504128373543,\r\n    \"content\": []\r\n}";
+	const response = Response.fromString(responseString);
+
+  it('Parses reseponse status code', function() {
   	response.statusCode.should.equal(200);
+  });
+
+  it('Parses reseponse status message', function() {
   	response.statusMessage.should.equal("OK");
+  });
+
+  it('Parses reseponse update timestamp', function() {
   	response.lastUpdated.getTime().should.equal(1504128373543);
+  });
+
+  it('Parses reseponse content', function() {
   	response.content.length.should.equal(0);
   });
+
 });
